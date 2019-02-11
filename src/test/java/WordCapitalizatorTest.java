6566d4bfc4ccfe3bc3e066474f57b0846d78f1ed
Arkady Sapozhnikov <arksap2002@gmail.com>
2019-02-11 16:51:07 +0300

    Tests update

--- src/test/java/WordCapitalizatorTest.java
+++ src/test/java/WordCapitalizatorTest.java
@@ -7,5 +7,7 @@ public class WordCapitalizatorTest {
         assertEquals("Hello, World", WordCapitalizator.capitalizeWords("heLlo, woRld"));
         assertEquals("Vasya , Vasya", WordCapitalizator.capitalizeWords("vasYa , Vasya"));
         assertEquals(" Vasya - Vasya ", WordCapitalizator.capitalizeWords(" vasya - vasya "));
+		assertEquals("", WordCapitalizator.capitalizeWords(""));
+        assertEquals("Vasya-Vasya", WordCapitalizator.capitalizeWords("vasya-vasYa"));
     }
 }
