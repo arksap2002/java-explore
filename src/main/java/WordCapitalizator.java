128bddac26bbfbb9c92f26b094b49db260879510
Arkady Sapozhnikov <arksap2002@gmail.com>
2019-02-18 22:20:53 +0300

    Update WordCapitalizator.java

--- src/main/java/WordCapitalizator.java
+++ src/main/java/WordCapitalizator.java
@@ -2,15 +2,11 @@ public class WordCapitalizator {
     public static String capitalizeWords(String words) {
         char[] chars = words.toCharArray();
         for (int i = 0; i < chars.length; i++) {
-            if ((i == 0 || (!is_letter(chars[i - 1]))) && is_letter(chars[i]))
+            if ((i == 0 || (!Character.isLetter(chars[i - 1]))) && Character.isLetter(chars[i]))
                 chars[i] = Character.toUpperCase(chars[i]);
-            if (i != 0 && is_letter(chars[i - 1]))
+            if (i != 0 && Character.isLetter(chars[i - 1]))
                 chars[i] = Character.toLowerCase(chars[i]);
         }
         return new String(chars);
-    }
-
-    private static boolean is_letter(char ch) {
-        return (ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z');
     }
 }
