21a7f5515ac6d38f362ddffba551202a0cc615b0
arksap2002 <47223481+arksap2002@users.noreply.github.com>
2019-02-21 19:07:57 +0300

    Crushing tests

--- src/test/java/WordCapitalizatorTest.java
+++ src/test/java/WordCapitalizatorTest.java
@@ -7,5 +7,6 @@ public class WordCapitalizatorTest {
         assertEquals("Hello, World", WordCapitalizator.capitalizeWords("heLlo, woRld"));
         assertEquals("Vasya , Vasya", WordCapitalizator.capitalizeWords("vasYa , Vasya"));
         assertEquals(" Vasya - Vasya ", WordCapitalizator.capitalizeWords(" vasya - vasya "));
+        assertEquals(" a", WordCapitalizator.capitalizeWords(" a"));
     }
 }
