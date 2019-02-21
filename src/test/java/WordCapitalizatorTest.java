eb9e3f9fd389d007b242f342f7a12f6355ed9d87
arksap2002 <47223481+arksap2002@users.noreply.github.com>
2019-02-21 19:15:48 +0300

    Update WordCapitalizatorTest.java

--- src/test/java/WordCapitalizatorTest.java
+++ src/test/java/WordCapitalizatorTest.java
@@ -7,7 +7,8 @@ public class WordCapitalizatorTest {
         assertEquals("Hello, World", WordCapitalizator.capitalizeWords("heLlo, woRld"));
         assertEquals("Vasya , Vasya", WordCapitalizator.capitalizeWords("vasYa , Vasya"));
         assertEquals(" Vasya - Vasya ", WordCapitalizator.capitalizeWords(" vasya - vasya "));
-	    assertEquals("", WordCapitalizator.capitalizeWords(""));
+	assertEquals("", WordCapitalizator.capitalizeWords(""));
         assertEquals("Vasya-Vasya", WordCapitalizator.capitalizeWords("vasya-vasYa"));
+	assertEquals(" a", WordCapitalizator.capitalizeWords(" a"));
     }
 }
