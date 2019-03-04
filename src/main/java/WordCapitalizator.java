941b3b06a5e94a5d5076cf6a5da5016d0b49a031
Arkady Sapozhnikov <arksap2002@gmail.com>
2019-03-04 19:52:58 +0300

    Update pom

--- src/main/java/WordCapitalizator.java
+++ src/main/java/WordCapitalizator.java
@@ -1,22 +1,16 @@
 public class WordCapitalizator {
     public static String capitalizeWords(String words) {
+        words = words.toLowerCase();
         char[] chars = words.toCharArray();
-        if (('a' <= chars[0]) && (chars[0] <= 'z')) {
-            chars[0] = Character.toUpperCase(chars[0]);
-        }
-        for (int i = 0; i < chars.length - 1; i++) {
-            if (chars[i] == ' ') {
-                chars[i + 1] = Character.toUpperCase(chars[i + 1]);
-            } else {
-                if (('A' <= chars[0]) && (chars[0] <= 'Z')) {
-                    chars[i + 1] = Character.toLowerCase(chars[i + 1]);
+        for (int i = 0; i < chars.length; i++) {
+            if (Character.isLetter(chars[i])) {
+                if ((i == 0) || (!Character.isLetter(chars[i - 1]))) {
+                    chars[i] = Character.toUpperCase(chars[i]);
+                } else {
+                    chars[i] = Character.toLowerCase(chars[i]);
                 }
             }
         }
-        StringBuilder stringBuilder = new StringBuilder();
-        for (char aChar : chars) {
-            stringBuilder.append(aChar);
-        }
-        return stringBuilder.toString();
+        return new String(chars);
     }
 }
