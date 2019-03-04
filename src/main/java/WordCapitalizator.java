e1e2af53439e434dddd9f8a5bce66995b3e21800
Arkady Sapozhnikov <arksap2002@gmail.com>
2019-03-04 19:46:47 +0300

    Update

--- src/main/java/WordCapitalizator.java
+++ src/main/java/WordCapitalizator.java
@@ -3,8 +3,13 @@ public class WordCapitalizator {
         words = words.toLowerCase();
         char[] chars = words.toCharArray();
         for (int i = 0; i < chars.length; i++) {
-            if ((i == 0 || (!Character.isLetter(chars[i - 1]))) && Character.isLetter(chars[i]))
-                chars[i] = Character.toUpperCase(chars[i]);
+            if (Character.isLetter(chars[i])) {
+                if ((i == 0) || (!Character.isLetter(chars[i - 1]))) {
+                    chars[i] = Character.toUpperCase(chars[i]);
+                } else {
+                    chars[i] = Character.toLowerCase(chars[i]);
+                }
+            }
         }
         return new String(chars);
     }
