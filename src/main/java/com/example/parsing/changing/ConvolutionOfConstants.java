63d1f9a5e449b605fa7de6938a18eb325981ba5b
Arkady Sapozhnikov <arksap2002@gmail.com>
2019-04-25 15:26:15 +0300

    Javaparser learning

--- src/main/java/com/example/parsing/changing/ConvolutionOfConstants.java
+++ src/main/java/com/example/parsing/changing/ConvolutionOfConstants.java
@@ -2,7 +2,6 @@ package com.example.parsing.changing;
 
 import com.github.javaparser.JavaParser;
 import com.github.javaparser.ast.CompilationUnit;
-import com.github.javaparser.ast.body.VariableDeclarator;
 import com.github.javaparser.ast.expr.BinaryExpr;
 import com.github.javaparser.ast.expr.IntegerLiteralExpr;
 import com.github.javaparser.ast.visitor.VoidVisitorAdapter;
@@ -28,35 +27,42 @@ public class ConvolutionOfConstants {
             if (n.getLeft().isBinaryExpr()) {
                 visit(n.getLeft().asBinaryExpr(), javaParserFacade);
             }
+            if (n.getLeft().isEnclosedExpr() && n.getLeft().asEnclosedExpr().getInner().isBinaryExpr()){
+                visit(n.getLeft().asEnclosedExpr().getInner().asBinaryExpr(), javaParserFacade);
+            }
             if (n.getRight().isBinaryExpr()) {
                 visit(n.getRight().asBinaryExpr(), javaParserFacade);
             }
-            if (n.getLeft().isIntegerLiteralExpr() && n.getRight().isIntegerLiteralExpr()) {
+            if (n.getRight().isEnclosedExpr() && n.getRight().asEnclosedExpr().getInner().isBinaryExpr()){
+                visit(n.getRight().asEnclosedExpr().getInner().asBinaryExpr(), javaParserFacade);
+            }
+            if ((n.getLeft().isIntegerLiteralExpr() || (n.getLeft().isEnclosedExpr() && n.getLeft().asEnclosedExpr().getInner().isIntegerLiteralExpr())) && (n.getRight().isIntegerLiteralExpr() || (n.getRight().isEnclosedExpr() && n.getRight().asEnclosedExpr().getInner().isIntegerLiteralExpr()))) {
                 IntegerLiteralExpr integerLiteralExpr = new IntegerLiteralExpr();
+                IntegerLiteralExpr left = new IntegerLiteralExpr();
+                IntegerLiteralExpr right = new IntegerLiteralExpr();
+                if (n.getLeft().isIntegerLiteralExpr()){
+                    left = n.getLeft().asIntegerLiteralExpr();
+                }else{
+                    left = n.getLeft().asEnclosedExpr().getInner().asIntegerLiteralExpr();
+                }
+                if (n.getRight().isIntegerLiteralExpr()){
+                    right = n.getRight().asIntegerLiteralExpr();
+                }else{
+                    right = n.getRight().asEnclosedExpr().getInner().asIntegerLiteralExpr();
+                }
                 if (n.getOperator() == BinaryExpr.Operator.PLUS) {
-                    integerLiteralExpr.setInt(n.getLeft().asIntegerLiteralExpr().asInt() + n.getRight().asIntegerLiteralExpr().asInt());
+                    integerLiteralExpr.setInt(left.asInt() + right.asInt());
                 }
                 if (n.getOperator() == BinaryExpr.Operator.MINUS) {
-                    integerLiteralExpr.setInt(n.getLeft().asIntegerLiteralExpr().asInt() - n.getRight().asIntegerLiteralExpr().asInt());
+                    integerLiteralExpr.setInt(left.asInt() - right.asInt());
                 }
                 if (n.getOperator() == BinaryExpr.Operator.MULTIPLY) {
-                    integerLiteralExpr.setInt(n.getLeft().asIntegerLiteralExpr().asInt() * n.getRight().asIntegerLiteralExpr().asInt());
+                    integerLiteralExpr.setInt(left.asInt() * right.asInt());
                 }
                 if (n.getOperator() == BinaryExpr.Operator.DIVIDE) {
-                    integerLiteralExpr.setInt(n.getLeft().asIntegerLiteralExpr().asInt() / n.getRight().asIntegerLiteralExpr().asInt());
+                    integerLiteralExpr.setInt(left.asInt() / right.asInt());
                 }
                 n.replace(integerLiteralExpr);
-            }
-        }
-    }
-
-    static class Updating_variables extends VoidVisitorAdapter<JavaParserFacade> {
-
-        @Override
-        public void visit(VariableDeclarator n, JavaParserFacade javaParserFacade) {
-            super.visit(n, javaParserFacade);
-            if (n.getInitializer().get().isUnaryExpr()){
-                
             }
         }
     }
@@ -65,7 +71,6 @@ public class ConvolutionOfConstants {
         CompilationUnit compilationUnit = JavaParser.parse(IOUtils.resourceToString(filename, Charset.defaultCharset()));
         TypeSolver typeSolver = new CombinedTypeSolver(new ReflectionTypeSolver());
         compilationUnit.accept(new Finding_operators(), JavaParserFacade.get(typeSolver));
-        compilationUnit.accept(new Updating_variables(), JavaParserFacade.get(typeSolver));
         //
         return compilationUnit.toString();
     }
