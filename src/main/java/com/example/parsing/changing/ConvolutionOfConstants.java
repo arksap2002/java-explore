c84b169e1755b39b69d29a62b6a676dbfb104df6
Arkady Sapozhnikov <arksap2002@gmail.com>
2019-06-24 22:11:24 +0300

    Javaparser learning

--- src/main/java/com/example/parsing/changing/ConvolutionOfConstants.java
+++ src/main/java/com/example/parsing/changing/ConvolutionOfConstants.java
@@ -2,12 +2,10 @@ package com.example.parsing.changing;
 
 import com.github.javaparser.JavaParser;
 import com.github.javaparser.ast.CompilationUnit;
-import com.github.javaparser.ast.Node;
 import com.github.javaparser.ast.expr.BinaryExpr;
 import com.github.javaparser.ast.expr.EnclosedExpr;
 import com.github.javaparser.ast.expr.IntegerLiteralExpr;
 import com.github.javaparser.ast.visitor.VoidVisitorAdapter;
-import com.github.javaparser.resolution.declarations.ResolvedValueDeclaration;
 import com.github.javaparser.symbolsolver.JavaSymbolSolver;
 import com.github.javaparser.symbolsolver.javaparsermodel.JavaParserFacade;
 import com.github.javaparser.symbolsolver.javaparsermodel.declarations.JavaParserFieldDeclaration;
@@ -18,6 +16,7 @@ import org.apache.commons.io.IOUtils;
 
 import java.io.IOException;
 import java.nio.charset.Charset;
+import java.util.Random;
 
 import static com.github.javaparser.ast.Node.SYMBOL_RESOLVER_KEY;
 
@@ -26,14 +25,24 @@ public class ConvolutionOfConstants {
 
     public static void main(String[] args) throws IOException {
         System.out.println(transformResource("/input5.java"));
+//        System.out.println(Generating_test());
     }
 
-    static class Finding_binary extends VoidVisitorAdapter<JavaParserFacade> {
+
+    static class Finding extends VoidVisitorAdapter<JavaParserFacade> {
         @Override
         public void visit(BinaryExpr n, JavaParserFacade javaParserFacade) {
             super.visit(n, javaParserFacade);
+            IntegerLiteralExpr integerLiteralExpr = new IntegerLiteralExpr();
+            integerLiteralExpr.setInt(0);
+            if ((n.getOperator() == BinaryExpr.Operator.MINUS) && (n.getRight().isNameExpr()) && (n.getLeft().isNameExpr())) {
+                if ((((JavaParserFieldDeclaration) (n.getRight().asNameExpr().resolve())).getWrappedNode()).equals(((JavaParserFieldDeclaration) (n.getLeft().asNameExpr().resolve())).getWrappedNode())) {
+                    n.replace(integerLiteralExpr);
+                    change = true;
+                }
+            }
             if (n.getLeft().isIntegerLiteralExpr() && n.getRight().isIntegerLiteralExpr()) {
-                IntegerLiteralExpr integerLiteralExpr = new IntegerLiteralExpr();
+                integerLiteralExpr = new IntegerLiteralExpr();
                 IntegerLiteralExpr left = new IntegerLiteralExpr();
                 IntegerLiteralExpr right = new IntegerLiteralExpr();
                 if (n.getLeft().isIntegerLiteralExpr()) {
@@ -58,9 +67,7 @@ public class ConvolutionOfConstants {
                 change = true;
             }
         }
-    }
 
-    static class Finding_enclose extends VoidVisitorAdapter<JavaParserFacade> {
         @Override
         public void visit(EnclosedExpr n, JavaParserFacade javaParserFacade) {
             super.visit(n, javaParserFacade);
@@ -73,37 +80,37 @@ public class ConvolutionOfConstants {
         }
     }
 
-    static class Finding_binary_name extends VoidVisitorAdapter<JavaParserFacade> {
-        @Override
-        public void visit(BinaryExpr n, JavaParserFacade javaParserFacade) {
-            super.visit(n, javaParserFacade);
-            IntegerLiteralExpr integerLiteralExpr = new IntegerLiteralExpr();
-            integerLiteralExpr.setInt(0);
-            if ((n.getOperator() == BinaryExpr.Operator.MINUS) && (n.getRight().isNameExpr()) && (n.getLeft().isNameExpr())) {
-                ResolvedValueDeclaration resolvedValueDeclaration_r = n.getRight().asNameExpr().resolve();
-                ResolvedValueDeclaration resolvedValueDeclaration_l = n.getLeft().asNameExpr().resolve();
-                JavaParserFieldDeclaration javaParserFieldDeclaration_r = (JavaParserFieldDeclaration) (resolvedValueDeclaration_r);
-                JavaParserFieldDeclaration javaParserFieldDeclaration_l = (JavaParserFieldDeclaration) (resolvedValueDeclaration_l);
-                Node node_right = javaParserFieldDeclaration_r.getWrappedNode();
-                Node node_left = javaParserFieldDeclaration_l.getWrappedNode();
-                if (node_left.equals(node_right)) {
-                    n.replace(integerLiteralExpr);
-                    change = true;
-                }
-            }
-        }
-    }
-
     public static String transformResource(String filename) throws IOException {
         CompilationUnit compilationUnit = JavaParser.parse(IOUtils.resourceToString(filename, Charset.defaultCharset()));
         TypeSolver typeSolver = new CombinedTypeSolver(new ReflectionTypeSolver());
         compilationUnit.setData(SYMBOL_RESOLVER_KEY, new JavaSymbolSolver(typeSolver));
+        int number = 0;
         while (change) {
             change = false;
-            compilationUnit.accept(new Finding_binary(), JavaParserFacade.get(typeSolver));
-            compilationUnit.accept(new Finding_enclose(), JavaParserFacade.get(typeSolver));
-            compilationUnit.accept(new Finding_binary_name(), JavaParserFacade.get(typeSolver));
+            compilationUnit.accept(new Finding(), JavaParserFacade.get(typeSolver));
+            number += 1;
         }
+        System.out.println(number);
         return compilationUnit.toString();
+    }
+
+    public static String Generating_test() {
+        String[] words = {"a", "b", "c", "d", "e", "f", "g", "e", "k", "l", "m"};
+        String[] operators = {"+", "-", "*"};
+        Random random = new Random();
+        int number = 100;
+        return making_test(words, operators, random, number);
+    }
+
+    private static String making_test(String[] words, String[] operators, Random random, int number) {
+        if (number < 0) {
+            return "(" + Math.abs(random.nextInt()) % 11 + operators[Math.abs(random.nextInt()) % 3] + Math.abs(random.nextInt()) % 11 + ")";
+        }
+        number--;
+        if (random.nextInt() % 2 == 0) {
+            return "(" + making_test(words, operators, random, number) + operators[Math.abs(random.nextInt()) % 3] + Math.abs(random.nextInt()) % 11 + ")";
+        } else {
+            return "(" + Math.abs(random.nextInt()) % 11 + operators[Math.abs(random.nextInt()) % 3] + making_test(words, operators, random, number) + ")";
+        }
     }
 }
