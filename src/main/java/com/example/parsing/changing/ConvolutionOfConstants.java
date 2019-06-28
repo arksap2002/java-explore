e921260f98d05d44c8529f5fc684edf0ec014a5e
Arkady Sapozhnikov <arksap2002@gmail.com>
2019-06-28 15:52:50 +0300

    Javaparser learning

--- src/main/java/com/example/parsing/changing/ConvolutionOfConstants.java
+++ src/main/java/com/example/parsing/changing/ConvolutionOfConstants.java
@@ -194,19 +194,19 @@ public class ConvolutionOfConstants {
         String[] words = {"a", "b", "c", "d", "e", "f", "g", "e", "k", "l", "m"};
         String[] operators = {"+", "-", "*"};
         Random random = new Random();
-        int number = 100;
+        int number = 200;
         return making_test(words, operators, random, number);
     }
 
     private static String making_test(String[] words, String[] operators, Random random, int number) {
         if (number < 0) {
-            return "(" + Math.abs(random.nextInt()) % 11 + operators[Math.abs(random.nextInt()) % 3] + Math.abs(random.nextInt()) % 11 + ")";
+            return "(" + Math.abs(random.nextInt()) % 100 + operators[Math.abs(random.nextInt()) % 3] + Math.abs(random.nextInt()) % 100 + ")";
         }
         number--;
         if (random.nextInt() % 2 == 0) {
-            return "(" + making_test(words, operators, random, number) + operators[Math.abs(random.nextInt()) % 3] + Math.abs(random.nextInt()) % 11 + ")";
+            return "(" + making_test(words, operators, random, number) + operators[Math.abs(random.nextInt()) % 3] + words[Math.abs(random.nextInt()) % 11] + ")";
         } else {
-            return "(" + Math.abs(random.nextInt()) % 11 + operators[Math.abs(random.nextInt()) % 3] + making_test(words, operators, random, number) + ")";
+            return "(" + Math.abs(random.nextInt()) % 100 + operators[Math.abs(random.nextInt()) % 3] + making_test(words, operators, random, number) + ")";
         }
     }
 }
